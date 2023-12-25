739a8fe450483d633a0e9644af2a5d61dde05e64
pricop-alexandru <nophz506@gmail.com>
2023-12-25 17:47:20 +0200

    v0.1

--- game.h
+++ game.h
@@ -3,10 +3,10 @@
 #include<fstream>
 class Game {
 public:
+    virtual ~Game() {};
     virtual void run() = 0;
     virtual void read() = 0;
     virtual void write() const = 0;
-    virtual ~Game() {};
     friend std::istream& operator>>(std::istream& in, Game& obj){ obj.read(); return in;}
     friend std::ostream& operator<<(std::ostream& out, const Game& obj){ obj.write(); return out;}
 };
